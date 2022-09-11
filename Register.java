e4c1dec8002967c03dbc6b10742eddf53e2f015c
Adarshp23 <93076959+Adarshp23@users.noreply.github.com>
2022-09-11 19:35:20 +0530

    Add files via upload

---
+++ Register.java
@@ -0,0 +1,46 @@
+package Register.model;
+
+public class Register {
+	private String uname;
+	private String emailId;
+	private int mobno;
+	private String password;
+	private String confirmpassword;
+	public Register() {
+		
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public int getMobno() {
+		return mobno;
+	}
+	public void setMobno(int mobno) {
+		this.mobno = mobno;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmpassword() {
+		return confirmpassword;
+	}
+	public void setConfirmpassword(String confirmpassword) {
+		this.confirmpassword = confirmpassword;
+	}
+	
+	
+	
+
+}
