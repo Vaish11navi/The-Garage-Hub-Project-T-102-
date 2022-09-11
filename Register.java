7674fed3e40f34c856f57a6955095d2f1b252a95
HP-OMEN PPC <91759408+Shubham0000001@users.noreply.github.com>
2022-09-11 12:45:18 +0530

    Add files via upload

---
+++ Register.java
@@ -0,0 +1,56 @@
+package bank.model;
+
+public class Register {
+	private int rNo;
+	private String name;
+	private double accBal;
+	private String uname;
+	private String pass;
+	
+	public void register() {
+		
+	}
+
+	public int getrNo() {
+		return rNo;
+	}
+
+	public void setrNo(int rNo) {
+		this.rNo = rNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getAccBal() {
+		return accBal;
+	}
+
+	public void setAccBal(double accBal) {
+		this.accBal = accBal;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+
+}
