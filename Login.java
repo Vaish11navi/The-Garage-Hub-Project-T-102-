42ec6a6130e2e935aefe501be8202fdd4b9724e3
Adarshp23 <93076959+Adarshp23@users.noreply.github.com>
2022-09-11 13:37:33 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,23 @@
+package Login.model;
+
+public class Login {
+	private String emailId;
+	private String password;
+	public Login() {
+		
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
